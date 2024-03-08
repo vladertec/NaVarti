ebc4f2a1a5297479a55dba54f00d5ee1625fbf84
vladertec <sarnavsk2001@gmail.com>
2024-03-08 20:55:27 +0100

    with mobile make request component

--- client/src/components/MakeRequest/MakeRequest.jsx
+++ client/src/components/MakeRequest/MakeRequest.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import React from "react"
+import Slider from "react-slick"
+import ModeEditIcon from "@mui/icons-material/ModeEdit"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 const MakeRequest = ({ RequestItems }) => {
-  const requestButtonClick = (link) => {
-    window.location.href = link;
-  };
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1.68,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+  }
 
   return (
     <div className="make-request">
+      <p className="make-request__mobile-title">Зробити запит</p>
       <div className="make-request__container">
         <p className="make-request__title">Зробити запит</p>
         <div className="make-request__card-container">
@@ -16,21 +25,45 @@ const MakeRequest = ({ RequestItems }) => {
               <img
                 className="make-request__card-img"
                 src={item.photoUrl}
-                alt={item.photoUrl}
+                alt={item.title}
               />
               <h2 className="make-request__card-name">{item.title}</h2>
-              <p className="make-request__card-position">{item.position}</p>
               <p className="make-request__card-text">{item.text}</p>
-              <button className="make-request__btn" onClick={() => requestButtonClick(item.link)}>
-                Заповнити форму{" "}
-                <ModeEditIcon className="make-request__btn-icon" />{" "}
+              <button
+                className="make-request__btn"
+                onClick={() => (window.location.href = item.link)}
+              >
+                Заповнити форму
+                <ModeEditIcon className="make-request__btn-icon" />
               </button>
             </div>
           ))}
         </div>
+        <div className="make-request__mobile-slider">
+          <Slider {...sliderSettings}>
+            {RequestItems.map((item, index) => (
+              <div className="make-request__card" key={index}>
+                <img
+                  className="make-request__card-img"
+                  src={item.photoUrl}
+                  alt={item.title}
+                />
+                <h2 className="make-request__card-name">{item.title}</h2>
+                <p className="make-request__card-text">{item.text}</p>
+                <button
+                  className="make-request__btn"
+                  onClick={() => (window.location.href = item.link)}
+                >
+                  Заповнити форму
+                  <ModeEditIcon className="make-request__btn-icon" />
+                </button>
+              </div>
+            ))}
+          </Slider>
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default MakeRequest;
+export default MakeRequest

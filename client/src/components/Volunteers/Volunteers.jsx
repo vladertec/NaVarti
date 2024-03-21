96f41a9076a73146289959a3ad1e904f03eb3d80
vladertec <sarnavsk2001@gmail.com>
2024-03-21 15:02:17 +0100

    deploy release - 8

--- client/src/components/Volunteers/Volunteers.jsx
+++ client/src/components/Volunteers/Volunteers.jsx
@@ -40,7 +40,7 @@ const Volunteers = ({ volunteerPositionItems }) => {
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2300,
+    autoplaySpeed: 2000,
     cssEase: "linear",
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
